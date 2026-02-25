70f6196e18d0c7343a974ddefdcf76c0bed8f113
ChamodyaPiyasarani <chamo.piyasarani2002@gmail.com>
2026-02-25 13:11:17 +0530

    Initial Commit

---
+++ ClinicFlow/ClinicFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlowApp.swift
+//  ClinicFlow
+//
+//  Created by cobsccomp242p-030 on 2026-02-25.
+//
+
+import SwiftUI
+
+@main
+struct ClinicFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
